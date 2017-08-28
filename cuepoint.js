238aed12e29931523816fb5a49d2fe0543f81426
arpitmaheshwari <arpitix@gmail.com>
2017-08-28 17:04:10 +0530

    Styling for demo

--- cuepoint.js
+++ cuepoint.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
         player = flowplayer("#player");
-        console.dir(player);
-
 
         $("#transcript a").on("click", function(){
             player.seek(Number(this.attributes[0].value));
